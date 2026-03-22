10d89889c8d35e643882878e1e5d129bc7cda25b
Daniel Cruz <cruz.daniel.2003@gmail.com>
2026-03-22 14:54:39 -0500

    clasesDAO
    Se crean las clases DAO:
    *NominaDAO

--- src/main/java/ModeloDAO/NominaDAO.java
+++ src/main/java/ModeloDAO/NominaDAO.java
@@ -1,5 +1,18 @@
 package ModeloDAO;
 
-public class NominaDAO {
+import java.util.ArrayList;
+import java.util.List;
 
+import ModeloDTO.Nomina;
+
+public class NominaDAO {
+	public static List<Nomina> listaAspirantes = new ArrayList<>();
+	
+	public void guardar(Nomina n){
+        listaAspirantes.add(n);
+    }
+
+    public List<Nomina> listar(){
+        return listaAspirantes;
+    }
 }
